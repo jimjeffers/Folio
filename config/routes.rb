c8fbc380956c44f957020d2d14426bce64dd9c84
Jim Jeffers <jim@sumocreations.com>
2009-11-17 16:35:32 +1000

    Fixed inqiure route.

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ ActionController::Routing::Routes.draw do |map|
   map.spotlight '/work/:guid', :controller => 'site', :action => 'spotlight'
   map.work '/work', :controller => 'site', :action => 'work'
   map.about '/about', :controller => 'site', :action => 'about'
-  map.inquire '/inquire', :controller => 'site', :action => 'work'
+  map.inquire '/inquire', :controller => 'site', :action => 'inquire'
   
   # Defaults
   map.connect ':controller/:action/:id'
