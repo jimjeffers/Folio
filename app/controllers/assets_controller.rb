f2a6ad5da6280a6e37e208abd57a1771241f2dec
Jim Jeffers <jim@sumocreations.com>
2010-05-17 16:13:48 +1000

    Added jcropping functionality.

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -3,10 +3,36 @@ class AssetsController < InheritedResources::Base
   respond_to :html, :xml, :json
   belongs_to :project
   before_filter :login_required
+  actions :all, :except => [ :create, :update]
   
   def create
-    create! do |success,failure|
-      success.html { redirect_to @project }
+    @project = Project.find(params[:project_id])
+    @asset = User.new(params[:asset])
+    if @asset.save
+      flash[:notice] = 'Asset was successfully created.'
+      if params[:asset][:image].blank?
+        redirect_to @project
+      else
+        render :action => 'cropping'
+      end
+    else
+      render :action => 'new'
+    end
+  end
+  
+  # PUT /assets/1
+  def update
+    @project = Project.find(params[:project_id])
+    @asset = Asset.find(params[:id])
+    if @asset.update_attributes(params[:asset])
+      flash[:notice] = 'Asset was successfully updated.'
+      if params[:asset][:image].blank?
+        redirect_to(@project)
+      else
+        render :action => 'cropping'
+      end
+    else
+      render :action => "edit"
     end
   end
   
--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,6 +1,22 @@
 class Asset < ActiveRecord::Base
+  # Constants
+  IMAGE_SSW = 940
+  IMAGE_SSH = 1000
+  IMAGE_PW = 940
+  IMAGE_PH = 400
+  IMAGE_SCW = 220
+  IMAGE_SCH = 140
+  IMAGE_TNW = 100
+  IMAGE_TNH = 75
+  
   # Plugins
-  has_attached_file :image, :styles => { :screenshot => "940x1000", :preview => "940x400#", :showcase => "220x140#", :thumbnail => "100x75#" }
+  has_attached_file :image,
+        :styles => { :screenshot => ["#{IMAGE_SSW}x#{IMAGE_SSH}>", :jpg],
+                     :preview => ["#{IMAGE_PW}x#{IMAGE_PH}#", :jpg],
+                     :showcase => ["#{IMAGE_SCW}x#{IMAGE_SCH}#", :jpg],
+                     :thumbnail => ["#{IMAGE_TNW}x#{IMAGE_TNH}#", :jpg] },
+        :processors => [:jcropper],
+        :default_url => "/images/default_image.png"
   has_guid :name
   
   # Relationships
@@ -8,7 +24,29 @@ class Asset < ActiveRecord::Base
   
   # Validations
   validates_presence_of :alt_text
+  validates_attachment_content_type :image, :content_type => ['image/jpeg', 'image/pjpeg', 'image/jpg', 'image/png']
   
   # Scopes
   default_scope :order => "assets.position ASC, assets.name ASC"
+
+  # Hooks
+  after_update :reprocess_image, :if => :cropping?
+  
+  # Accessors
+  attr_accessor :crop_x, :crop_y, :crop_w, :crop_h
+  
+  def cropping?
+    !crop_x.blank? && !crop_y.blank? && !crop_w.blank? && !crop_h.blank?
+  end
+    
+  def image_geometry(style = :original)
+    @geometry ||= {}
+    @geometry[style] ||= Paperclip::Geometry.from_file image.path(style)
+  end
+  
+  private
+  
+  def reprocess_image
+    image.reprocess!
+  end
 end
